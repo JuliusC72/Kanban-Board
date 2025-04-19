68cbe1fe555d32d4fc8239b30b57df346abee5a1
JuliusC72 <72jules@gmail.com>
2025-04-18 18:15:06 -0600

    error messages

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,13 +1,15 @@
 import { useState, FormEvent, ChangeEvent } from "react";
-
+import { useNavigate } from "react-router-dom";
 import Auth from '../utils/auth';
 import { login } from "../api/authAPI";
 
 const Login = () => {
+  const navigate = useNavigate();
   const [loginData, setLoginData] = useState({
     username: '',
     password: ''
   });
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
@@ -19,11 +21,15 @@ const Login = () => {
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
+    setErrorMessage('');
+    
     try {
       const data = await login(loginData);
       Auth.login(data.token);
+      navigate('/');
     } catch (err) {
       console.error('Failed to login', err);
+      setErrorMessage('Invalid username or password');
     }
   };
 
@@ -31,25 +37,27 @@ const Login = () => {
     <div className='container'>
       <form className='form' onSubmit={handleSubmit}>
         <h1>Login</h1>
-        <label >Username</label>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+        <label>Username</label>
         <input 
           type='text'
           name='username'
           value={loginData.username || ''}
           onChange={handleChange}
+          required
         />
-      <label>Password</label>
+        <label>Password</label>
         <input 
           type='password'
           name='password'
           value={loginData.password || ''}
           onChange={handleChange}
+          required
         />
-        <button type='submit'>Submit Form</button>
+        <button type='submit'>Login</button>
       </form>
     </div>
-    
-  )
+  );
 };
 
 export default Login;
